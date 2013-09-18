c36dd41b01ea832c94f5e03f72604a6a09d3a8fa
Alexandr Ivanuna <ivanuna@gmail.com>
2013-09-18 10:45:08 +0300

    begin scaffolded app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AutoApp::Application.config.secret_token = '1afa6ac2ca90c52452c7ceebfd441dfa1473a085ffd048f44d8207baf0517a3a1562df897aed13aaf5231cd5323dd95e3acbd890ad63fda940fad9f107158e87'
